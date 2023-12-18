3bbb524bc29a1737fefaa2b7e67c89e47bee5cee
aborie <antoine.borie@soprasteria.com>
2023-12-19 00:33:18 +0100

    :fix: Passage vers le Hashrouter car gh-client ne supporte pas le BrowserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
 import './App.css'
-import {Route, Routes} from "react-router-dom";
+import {HashRouter, Route, Routes} from "react-router-dom";
 import QuizQuestionsPage from "./pages/quiz-questions.page.tsx";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import QuizResultPage from "./pages/quiz-result.page.tsx";
 
 function App() {
     return (
-        <Routes>
-            <Route path="/" element={<QuizQuestionsPage/>}/>
-            <Route path="/result" element={<QuizResultPage/>}/>
-        </Routes>
-
+        <HashRouter>
+            <Routes>
+                <Route path="/" element={<QuizQuestionsPage/>}/>
+                <Route path="/result" element={<QuizResultPage/>}/>
+            </Routes>
+        </HashRouter>
     )
 }
 
